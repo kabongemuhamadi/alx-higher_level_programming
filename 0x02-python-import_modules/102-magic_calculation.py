2c2c7a4f3922c82d847d3250a7113001e2435f69
kabongemuhamadi <56195790+kabongemuhamadi@users.noreply.github.com>
2022-12-02 02:23:59 +0300

    changed

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
-
-from magic_calculation_102 import add, sub
-
-
-def magic_calculatrice(a, b):
+def magic_calculation(a, b):
+    from magic_calculation_102 import add, sub
     if a < b:
         c = add(a, b)
-        for loop in range(38, 98):
+        for i in range(4, 6):
+            c = add(c, i)
+        return (c)
+    else:
+        return (sub(a, b))
